3b70ac9d57ff24d517710d2cb084c01f276e6c06
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-04-14 16:29:57 -0300

    fix: corrigir posicionamento da imagem home canto esquerdo superior

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -32,21 +32,23 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.containerHome}>
+    <div className={styles.page}>
       <div
         className={styles.dots}
         style={{ backgroundImage: `url(${dots})` }}
-      ></div>{" "}
-      {/* Pontinhos no fundo */}
-      <div className={styles.home}>
-        <img src={gitLogo} alt="" />
-        <SearchBar
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-          onSearch={handleSearch}
-        />
-        {error && <ErrorMessage message={error} />}
-        {userData && <UserCard data={userData} />}
+      ></div>
+      <div className={styles.containerHome}>
+        {/* Pontinhos no fundo */}
+        <div className={styles.home}>
+          <img src={gitLogo} alt="" />
+          <SearchBar
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            onSearch={handleSearch}
+          />
+          {error && <ErrorMessage message={error} />}
+          {userData && <UserCard data={userData} />}
+        </div>
       </div>
     </div>
   );
